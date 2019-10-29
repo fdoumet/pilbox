0edaa947ea52b6592d83fa200669069ea0c10abb
Francis <fdoumet@gmail.com>
2019-10-29 15:43:40 -0700

    Newer versions of tornado don't include `_callbacks` or `_timeouts`

--- pilbox/app.py
+++ pilbox/app.py
@@ -401,7 +401,7 @@ def sig_handler(server, sig, frame):
 
     def stop_loop(deadline):
         now = time.time()
-        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+        if now < deadline: #and (io_loop._callbacks or io_loop._timeouts):
             logging.info('Waiting for next tick')
             io_loop.add_timeout(now + 1, stop_loop, deadline)
         else:
@@ -410,6 +410,7 @@ def sig_handler(server, sig, frame):
 
     def shutdown():
         logging.info('Stopping http server')
+        server.close_all_connections()
         server.stop()
         logging.info('Will shutdown in %s seconds ...', MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
         stop_loop(time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
